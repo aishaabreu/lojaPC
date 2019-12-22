e37f9fbec08c808ae4b54dbbfbdb9ed63c92ae21
Felipe Rian <feliperian@users.noreply.github.com>
2019-12-22 04:57:03 -0300

    Adicioando testes unitarios dos pedidos

--- api/pedidos/tests.py
+++ api/pedidos/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class PedidoAdminTest(TestCase):
+    def test_posso_montar_um_computador(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_escolher_uma_placa_nao_suportada(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_montar_computador_sem_memoria_ram(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_exceder_os_slots_de_memoria(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_exceder_a_memoria_suportada(self):
+        self.fail("Incompleto")
+
+    def test_placa_de_video_obrigatorio_quando_nao_possui_video_integrado(self):
+        self.fail("Incompleto")
+
+
+class PedidoAPITest(TestCase):
+    def test_posso_montar_um_computador(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_escolher_uma_placa_nao_suportada(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_montar_computador_sem_memoria_ram(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_exceder_os_slots_de_memoria(self):
+        self.fail("Incompleto")
+
+    def test_nao_posso_exceder_a_memoria_suportada(self):
+        self.fail("Incompleto")
+
+    def test_placa_de_video_obrigatorio_quando_nao_possui_video_integrado(self):
+        self.fail("Incompleto")
--- api/produtos/tests.py
+++ api/produtos/tests.py
@@ -13,64 +13,64 @@ class ProdutosAdminTest(TestCase):
 
     def test_posso_cadastrar_um_processador(self):
         data = {
-            'descricao': 'Processador de Teste',
-            'marca': Processador.AMD
+            "descricao": "Processador de Teste",
+            "marca": Processador.AMD
         }
 
         response = self.client.post(
-            reverse_lazy('admin:produtos_processador_add'), data)
+            reverse_lazy("admin:produtos_processador_add"), data)
         
         processador = Processador.objects.get()
-        self.assertEqual(processador.descricao, data['descricao'])
-        self.assertEqual(processador.marca, data['marca'])
+        self.assertEqual(processador.descricao, data["descricao"])
+        self.assertEqual(processador.marca, data["marca"])
 
     def test_posso_cadastrar_uma_placa_mae(self):
         processador = Processador.objects.create(
-            descricao='Processador de Teste',
+            descricao="Processador de Teste",
             marca=Processador.AMD
         )
 
         data = {
-            'descricao': 'Placa Mãe de Teste',
-            'processadores_suportados': [processador.pk],
-            'slots_memoria': 2,
-            'memoria_suportaca': 32,
-            'video_integrado': False
+            "descricao": "Placa Mãe de Teste",
+            "processadores_suportados": [processador.pk],
+            "slots_memoria": 2,
+            "memoria_suportaca": 32,
+            "video_integrado": False
         }
 
         response = self.client.post(
-            reverse_lazy('admin:produtos_placamae_add'), data)
+            reverse_lazy("admin:produtos_placamae_add"), data)
 
         placa = PlacaMae.objects.get()
-        self.assertEqual(placa.descricao, data['descricao'])
+        self.assertEqual(placa.descricao, data["descricao"])
         self.assertEqual(
-            list(placa.processadores_suportados.values_list('pk', flat=True)),
-            data['processadores_suportados']
+            list(placa.processadores_suportados.values_list("pk", flat=True)),
+            data["processadores_suportados"]
         )
-        self.assertEqual(placa.slots_memoria, data['slots_memoria'])
-        self.assertEqual(placa.memoria_suportaca, data['memoria_suportaca'])
-        self.assertEqual(placa.video_integrado, data['video_integrado'])
+        self.assertEqual(placa.slots_memoria, data["slots_memoria"])
+        self.assertEqual(placa.memoria_suportaca, data["memoria_suportaca"])
+        self.assertEqual(placa.video_integrado, data["video_integrado"])
 
     def test_posso_cadastrar_uma_memoria_ram(self):
         data = {
-            'descricao': 'Memória de Teste',
-            'tamanho': 8
+            "descricao": "Memória de Teste",
+            "tamanho": 8
         }
 
         response = self.client.post(
-            reverse_lazy('admin:produtos_memoriaram_add'), data)
+            reverse_lazy("admin:produtos_memoriaram_add"), data)
         
         memoria = MemoriaRam.objects.get()
-        self.assertEqual(memoria.descricao, data['descricao'])
-        self.assertEqual(memoria.tamanho, data['tamanho'])
+        self.assertEqual(memoria.descricao, data["descricao"])
+        self.assertEqual(memoria.tamanho, data["tamanho"])
 
     def test_posso_cadastrar_uma_placa_de_video(self):
         data = {
-            'descricao': 'Placa de Vídeo de Teste',
+            "descricao": "Placa de Vídeo de Teste",
         }
 
         response = self.client.post(
-            reverse_lazy('admin:produtos_placadevideo_add'), data)
+            reverse_lazy("admin:produtos_placadevideo_add"), data)
         
         placa = PlacaDeVideo.objects.get()
-        self.assertEqual(placa.descricao, data['descricao'])
+        self.assertEqual(placa.descricao, data["descricao"])

ccb7ea513c741d022b9fc00c298bbe17c01d06c5
Felipe Rian <feliperian@users.noreply.github.com>
2019-12-22 05:21:45 -0300

    Adicionado pedidos de computadores na administracao

--- api/pedidos/admin.py
+++ api/pedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Computador
 
-# Register your models here.
+
+class ComputadorAdmin(admin.ModelAdmin):
+    filter_horizontal = ("memoria",)
+admin.site.register(Computador, ComputadorAdmin)

699a0ac61a4a68e84220c930c9824002768cea1a
Felipe Rian <feliperian@users.noreply.github.com>
2019-12-23 20:12:16 -0300

    Adicionado allowed hosts nao restritivo

--- loja/settings.py
+++ loja/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hmsojh!z5r33&5-g0(g=5f$2im(#+rx45g$f=$6zv&dm70nv)b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

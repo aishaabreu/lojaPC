66ba18851f30eeecab5804686256e0e982f86cb9
Felipe Rian <feliperian@users.noreply.github.com>
2019-12-23 14:17:29 -0300

    Adicionado redirecionaento para administracao

--- loja/urls.py
+++ loja/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from loja.router import router
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))

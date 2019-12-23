e091e6a02b4491feb3c1c6629170a99031ed3032
Felipe Rian <feliperian@users.noreply.github.com>
2019-12-23 20:07:34 -0300

    Resolvido erro de compatibilidade com django3 e whitenoise 4

--- loja/wsgi.py
+++ loja/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from whitenoise import WhiteNoise
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loja.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
